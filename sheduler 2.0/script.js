ac046c5d54ad82afc22c2749a3c150c1cda36bd6
br1tt00 <92647345+br1tt00@users.noreply.github.com>
2022-02-07 12:53:15 -0800

    Add files via upload

---
+++ sheduler 2.0/script.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+    for (let i = 9; i < 18; i++) {
+
+        //Row
+        let row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+        //Column 1
+        let col1 = $('<div class="col-sm-2"> <p class="hour">' + time(i) + '</p>');
+
+        //Column 2
+        let col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add schedule.."></textarea>`);
+
+        //Column 3
+        let col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+
+        //Row Append
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+
+        // Row/Container
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }
+
+// Time Setup
+    function time(hours) {
+        let AmPm = hours >= 24 ? 'Pm' : 'Am';
+        hours = hours % 24;
+        hours = hours ? hours : 24;
+        return hours + AmPm;
+    }
+
+    time();
+
+//Update Row Colors
+    function updateRows() {
+        let currentTime = new Date().getHours();
+
+        for (let i = 9; i < 18; i++) {
+            console.log(currentTime, $(`#${i}`).data("time"));
+
+            if ($(`#${i}`).data("time") == currentTime) {
+                $(`#text${i}`).addClass("present");
+
+            } else if (currentTime < $(`#${i}`).data("time")) {
+                $(`#text${i}`).addClass("future");
+            }
+        }
+    }
+
+    interval(function () {
+        updateRows();
+    }, 100);
+
+//Storage
+    function getLocalStorage(key) {
+        let value = localStorage.getItem(key);
+    
+        if (value) {
+            $(`#text${key}`).text(value);
+        }
+    }
+
+    let saveBtn = $('.saveBtn');
+
+    saveBtn.on('click', function () {
+        let event = $(this).attr('id');
+
+        let eventText = $(this).parent().siblings().children('.description').val();
+
+        localStorage.setItem(event, eventText);
+    });
+});
